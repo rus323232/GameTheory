03bb7f1aefe4cbaee03219136b2cec65c060ffc6
Ruslan <rus323232@gmail.com>
2016-05-16 17:13:26 +0400

    добавил решение уравнений

--- handler.php
+++ handler.php
@@ -1,17 +1,20 @@
 <?php 
 
+    include_once 'FindingUnknowVariables.php';
+
     if(isset($_POST['pay_matrix']) && isset($_POST['n']) && isset($_POST['m']) && isset($_POST['minmaxA']) && isset($_POST['maxminB'])) {
 
     $paymatrix = $_POST['pay_matrix'];
-    $minmaxA = isset($_POST['minmaxA']);
-    $maxminB = isset($_POST['maxminB']);
-    $n = isset($_POST['n']);
-    $m = isset($_POST['m']);
+    $minmaxA = $_POST['minmaxA'];
+    $maxminB = $_POST['maxminB'];
+    $n = $_POST['n'];
+    $m = $_POST['m'];
     
 
     echo json_encode($paymatrix[1][1]);
     exit;
 }
+   
 
 
 
